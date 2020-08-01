9161bc7b22c9872714e6e2170924fc3bd8cc47b3
mohannautiyal <mohannautiyal80@gmail.com>
2020-08-01 22:36:11 +0530

    ItestListener

--- TestNGProject/src/test/java/com/home/TestClass.java
+++ TestNGProject/src/test/java/com/home/TestClass.java
@@ -1,21 +1,26 @@
 package com.home;
 
+import static org.testng.Assert.assertTrue;
+
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import com.home.data.data;
+import com.home.listeners.Testlistener;
 
+@Listeners(Testlistener.class)
 public class TestClass {
 	
-	@BeforeSuite
-	public void BeforeSuite() {
-		System.out.println("Running before suite");
-	}
+	
+	  @BeforeSuite public void BeforeSuite() {
+	  System.out.println("Running before suite"); }
+	 
 	
 	@Test(dataProviderClass=data.class,dataProvider="getData")
 	public void TestData(String Param1,int Param2) {
 		System.out.println("Running with DataProvider "+Param1+"  and "+Param2);
-		
+		//assertTrue(false);
 	}
 
 }
--- TestNGProject/src/test/java/com/home/TestClassB.java
+++ TestNGProject/src/test/java/com/home/TestClassB.java
@@ -1,18 +1,28 @@
 package com.home;
 
+import static org.testng.Assert.assertTrue;
+
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+import com.home.listeners.Testlistener;
+
+
+
+
+
+@Listeners(Testlistener.class)
 public class TestClassB {
 
 	
 	  @BeforeSuite 
 	  public void BeforeSuite() {
 	  System.out.println("Running before suite next class"); }
-	 
+	  
 	
 	@Test
 	public void TestD() {
-		
+		assertTrue(true);
 	}
 }
